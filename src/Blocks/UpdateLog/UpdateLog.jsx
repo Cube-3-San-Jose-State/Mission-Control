5b6641cc017efa8213e9b7bc68140dc46d503c93
Carter-TE <cte12001@gmail.com>
2023-03-17 18:17:09 -0700

    Added HB error Co-authored-by: edwardsb1 <edwardsb1@users.noreply.github.com>

--- src/Blocks/UpdateLog/UpdateLog.jsx
+++ src/Blocks/UpdateLog/UpdateLog.jsx
@@ -4,23 +4,33 @@ class UpdateLog extends Component {
 		constructor(props){
 			super(props);
 			this.state = {
-				data: props.data,
+				logs: [],
 				displayString: ""
 			};
 		}
 
-		errorCheck(){
-			this.setState({displayString: "example error " + Math.random()});
-		}
+		
 
-		componentDidUpdate(){
-			this.errorCheck();
+		componentDidUpdate(previousProps){
+			let previousData = previousProps.data;
+			let currentData = this.props.data;
+
+			if (currentData["HEARTBEAT"] != previousData["HEARTBEAT"] + 1 ) {
+				let packetLost = currentData["HEARTBEAT"] - previousData["HEARTBEAT"];
+				console.log("TRANSMISSION ERROR at HB: "+previousData["HEARTBEAT"]+".\nTotal packets lost: "+packetLost);
+			}
 		}
 
 
     	render() { 
 			return (
-	    		<div>{this.state.data}</div>
+	    		<div>
+					<h1>Update Log</h1>
+					<div id="updateLog">
+						{...this.state.logs}
+					</div>
+				</div>
+
     		)
 		}
 }
